3ff8948343a6cc561d6813e160f97f05ab235925
Julián David <julianfullstackdev@gmail.com>
2024-08-12 00:00:01 -0500

    merge with base, fix issue with padding and max-width

--- components/atoms/container/container.tsx
+++ components/atoms/container/container.tsx
@@ -11,5 +11,5 @@ export const Container = ({
   children: React.ReactNode
   className?: string
 }) => {
-  return <div className={cn('px-4 sm:px-16 lg:px-20 xl:px-0', className)}>{children}</div>
+  return <div className={cn('px-4 sm:px-16 lg:px-20 2xl:px-0', className)}>{children}</div>
 }
