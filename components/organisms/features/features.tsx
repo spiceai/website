d8e66cb23b749b996d19a0e132872e3943353bff
Julián David <julianfullstackdev@gmail.com>
2024-08-13 01:40:33 -0500

    add responsive, fix articles margin on mobile

--- components/organisms/features/features.tsx
+++ components/organisms/features/features.tsx
@@ -3,19 +3,19 @@ import { Feature } from 'components/molecules/feature/feature'
 
 export const Features = () => {
   return (
-    <section className='mx-auto mt-20 max-w-[843px]'>
+    <section className='mx-auto mt-20 px-6 md:max-w-[843px] md:px-0'>
       <Title className='mb-14'>
         We believe you can have more control,{' '}
         <span className='text-primary'>accelerate AI adoption</span>, & do it more securely and at a
         lower cost than you are doing today.
       </Title>
 
-      <div className='flex gap-6'>
-        <div className='flex w-1/2 flex-col gap-6'>
+      <div className='flex flex-col gap-6 md:flex-row'>
+        <div className='flex w-full flex-col gap-6 md:w-1/2'>
           <Feature />
           <Feature />
         </div>
-        <div className='flex w-1/2 flex-col gap-6 pt-20'>
+        <div className='flex w-full flex-col gap-6 md:w-1/2 md:pt-20'>
           <Feature />
           <Feature />
         </div>
