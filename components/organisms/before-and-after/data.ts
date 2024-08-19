0344818afddd051ebc1734c9b77493e85557d763
Julián David <48492084+hipdev@users.noreply.github.com>
2024-08-18 23:06:38 -0500

    add a slider to fullfill huge resolution screens

--- components/organisms/before-and-after/data.ts
+++ components/organisms/before-and-after/data.ts
@@ -61,5 +61,23 @@ export const beforeAndAfterSlides: SlideData[] = [
         textAfter: 'Spice did all the hard work of connecting the app to data.'
       }
     ]
+  },
+  {
+    imageBefore: '/before-demo.png',
+    imageAfter: '/after-demo.png',
+    benefits: [
+      {
+        textBefore: 'Without Spice, connect the data is a huge challenge.',
+        textAfter: 'Spice did all the hard work of connecting the app to data.'
+      },
+      {
+        textBefore: 'Without Spice, connect the data is a huge challenge.',
+        textAfter: 'Spice did all the hard work of connecting the app to data.'
+      },
+      {
+        textBefore: 'Without Spice, connect the data is a huge challenge.',
+        textAfter: 'Spice did all the hard work of connecting the app to data.'
+      }
+    ]
   }
 ]
