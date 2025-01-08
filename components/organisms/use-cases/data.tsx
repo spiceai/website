cba40e93c9fb23cd25a236a80b9928c1e64d359a
Luke Kim <80174+lukekim@users.noreply.github.com>
2025-01-08 13:37:25 -0800

    Re-order

--- components/organisms/use-cases/data.tsx
+++ components/organisms/use-cases/data.tsx
@@ -14,18 +14,18 @@ export const dataUseCases: UseCaseData[] = [
     buttonLink: 'https://github.com/spiceai/cookbook/blob/trunk/openai_sdk/README.md'
   },
   {
-    title: 'Database CDN',
-    description:
-      'Co-locate working sets of data in Arrow, SQLite, and DuckDB with applications for fast, sub-second query.',
-    buttonText: 'Accelerating data with DuckDB',
-    buttonLink: 'https://github.com/spiceai/cookbook/blob/trunk/duckdb/accelerator/README.md'
-  },
-  {
     title: 'Retrieval-Augmented Generation (RAG)',
     description:
       'Ensure AI is grounded in data with high-performance search and text-to-SQL, across a semantic knowledge layer.',
     buttonText: 'Text-to-SQL',
     buttonLink: 'https://github.com/spiceai/cookbook/blob/trunk/text-to-sql/README.md'
+  },
+  {
+    title: 'Database CDN',
+    description:
+      'Co-locate working sets of data in Arrow, SQLite, and DuckDB with applications for fast, sub-second query.',
+    buttonText: 'Accelerating data with DuckDB',
+    buttonLink: 'https://github.com/spiceai/cookbook/blob/trunk/duckdb/accelerator/README.md'
   },
   {
     title: 'Distributed Data Mesh',
